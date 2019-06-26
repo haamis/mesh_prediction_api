9f2540c22192cc4b43b481867e13322b4b12b5d7
Antti Virtanen <haama92@gmail.com>
2019-06-26 23:06:49 +0300

    More info returned for /articles, cleanup, bug fixes.

--- api.py
+++ api.py
@@ -5,20 +5,21 @@ app = Flask(__name__)
 
 db = sqlite3.connect("../test.db", check_same_thread=False)
 db.row_factory = sqlite3.Row
-#c = db.cursor()
 
 @app.route('/', methods=['GET'])
 def root():
+
     response = {}
     response["articles"] = "params: affiliation, author_name, mesh"
     response["authors"] = "params: affiliation, author_name, mesh"
     response["article_info"] = "params: pubmed_id"
+
     return jsonify(response)
+
 
 @app.route('/articles', methods=["GET"])
 def articles():
 
-    print(request.args)
     sql = "SELECT DISTINCT abstract, pub_year, articles.pubmed_id, title FROM articles \
             INNER JOIN article_authors ON articles.pubmed_id=article_authors.pubmed_id \
             INNER JOIN article_mesh ON articles.pubmed_id=article_mesh.pubmed_id WHERE "
@@ -41,7 +42,7 @@ def articles():
 
     # Default limit. Start from 0, return first 100 results
     limit_start = 0
-    limit_size = 100
+    limit_size = 20
 
     if request.args.get("per_page"):
         limit_size = request.args.get("per_page")
@@ -53,19 +54,36 @@ def articles():
     sql += " ORDER BY articles.pub_year"
     sql += " LIMIT " + str(limit_start) + ", " + str(limit_size)
 
-    print(params)
-    print(sql)
-    rows = db.execute(sql, params).fetchall()
+    article_rows = db.execute(sql, params).fetchall()
+    
+    # Change the sqlite3 Rows into dicts so they're mutable and JSON-able.
+    article_rows = [dict(zip(row.keys(), row)) for row in article_rows]
 
-    # Change the sqlite3 Row into a dict so it's JSON-able.
-    return jsonify([dict(zip(row.keys(), row)) for row in rows ])
+    author_rows_list = []
+    for row in article_rows:
+        author_rows = db.execute("SELECT f_name, l_name, affiliation FROM article_authors WHERE pubmed_id=?",
+                            (row["pubmed_id"],)).fetchall()
+        author_rows_list.append(author_rows)
+
+    mesh_terms_list = []
+    for row in author_rows:
+        mesh_terms = db.execute("SELECT DISTINCT group_concat(mesh, ';') FROM article_mesh \
+                                INNER JOIN article_authors ON article_mesh.pubmed_id=article_authors.pubmed_id \
+                                WHERE (article_authors.f_name LIKE ? AND article_authors.l_name LIKE ?)",
+                                (row["f_name"], row["l_name"])).fetchone()
+        mesh_terms = mesh_terms[0].split(";")
+        mesh_terms = sorted(list(set(mesh_terms)))
+        mesh_terms_list.append(mesh_terms)
+
+    for article, author_rows in zip(article_rows, author_rows_list):
+        article["authors"] = [ {"f_name": author["f_name"], "l_name": author["l_name"],
+                                "affiliation": author["affiliation"]} for author in author_rows ]
+
+    return jsonify(article_rows)
+
 
 @app.route("/authors", methods=["GET"])
 def authors():
-
-    # sql = "SELECT DISTINCT authors.f_name, authors.l_name FROM authors \
-    #         INNER JOIN article_authors ON authors.f_name=article_authors.f_name \
-    #         INNER JOIN article_mesh ON article_authors.pubmed_id=article_mesh.pubmed_id WHERE "
 
     sql = "SELECT DISTINCT authors.f_name, authors.l_name FROM authors \
             INNER JOIN article_authors ON authors.f_name=article_authors.f_name \
@@ -79,69 +97,54 @@ def authors():
         params.append('%' + request.args.get("affiliation") + '%')
 
     if request.args.get("author_name"):
-        #where_strs.append("(authors.f_name || ' ' || authors.l_name LIKE ?)")
-        #params.append('%' + request.args.get("author_name") + '%')
         where_strs.append("(authors.f_name LIKE ? OR authors.l_name LIKE ?)")
         params.extend([request.args.get("author_name")] * 2)
 
     for mesh in request.args.getlist("mesh"):
-        where_strs.append("articles.pubmed_id IN (SELECT pubmed_id FROM article_mesh WHERE mesh = ?)")
+        where_strs.append("article_mesh.pubmed_id IN (SELECT pubmed_id FROM article_mesh WHERE mesh = ?)")
         params.append(mesh)
 
-    # Default limit. Start from 0, return first 100 results
+    # Default limit. Start from 0, return first 20 results
     limit_start = 0
-    limit_size = 100
+    limit_size = 20
 
     if request.args.get("per_page"):
-        limit_size = request.args.get("per_page")
+        limit_size = int(request.args.get("per_page"))
 
     if request.args.get("page_number"):
-        limit_start = request.args.get("page_number") * limit_size
+        # `page_number - 1` to start page numbering from 1.
+        limit_start = (int(request.args.get("page_number")) - 1) * (limit_size)
     
     sql += " AND ".join(where_strs)
     #sql += " ORDER BY authors.l_name"
     sql += " LIMIT " + str(limit_start) + ", " + str(limit_size)
 
-    print(params)
-    print(sql)
-
-    rows = db.execute(sql, params).fetchall()
+    author_rows = db.execute(sql, params).fetchall()
 
     mesh_terms_list = []
-
-    for row in rows:
-        print(row[0], row[1])
+    for row in author_rows:
         mesh_terms = db.execute("SELECT DISTINCT group_concat(mesh, ';') FROM article_mesh \
                                 INNER JOIN article_authors ON article_mesh.pubmed_id=article_authors.pubmed_id \
                                 WHERE (article_authors.f_name LIKE ? AND article_authors.l_name LIKE ?)",
-                                (row[0], row[1])).fetchone()
+                                (row["f_name"], row["l_name"])).fetchone()
         mesh_terms = mesh_terms[0].split(";")
         mesh_terms = sorted(list(set(mesh_terms)))
-        print(mesh_terms)
         mesh_terms_list.append(mesh_terms)
 
-    #print("mesh_list", mesh_terms_list)
+    author_rows = [dict(zip(row.keys(), row)) for row in author_rows]
 
-    authors = [dict(zip(row.keys(), row)) for row in rows]
-
-    for author, mesh_terms in zip(authors, mesh_terms_list):
+    for author, mesh_terms in zip(author_rows, mesh_terms_list):
         author["mesh"] = mesh_terms
 
-    response = {}
-    response["authors"] = authors
-    response["mesh"] = mesh_terms
+    return jsonify(author_rows)
 
-    return jsonify(list(authors))
-
-    # Change the sqlite3 Row into a dict so it's JSON-able.
-    #return jsonify([dict(zip(row.keys(), row)) for row in rows ])
 
 @app.route("/article_info", methods=["GET"])
 def article_info():
-    authors = db.execute("SELECT f_name, l_name, affiliation FROM article_authors WHERE pubmed_id=?",
+    author_rows = db.execute("SELECT f_name, l_name, affiliation FROM article_authors WHERE pubmed_id=?",
                         (request.args.get("pubmed_id"),)).fetchall()
-    authors = [{"f_name": author[0], "l_name": author[1], "affiliation": author[2]} for author in authors]
-    print(authors)
+    author_rows = [{"f_name": author[0], "l_name": author[1], "affiliation": author[2]} for author in author_rows]
+    print(author_rows)
 
     mesh_terms = db.execute("SELECT group_concat(mesh, ';') FROM article_mesh WHERE pubmed_id=?",
                             (request.args.get("pubmed_id"),)).fetchone()
@@ -153,7 +156,7 @@ def article_info():
     print(info)
 
     response = {thing[0]: thing[1] for thing in zip(info.keys(), info)}
-    response["authors"] = authors
+    response["authors"] = author_rows
     response["mesh"] = mesh_terms
 
     return jsonify(response)
